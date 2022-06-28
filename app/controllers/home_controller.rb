66ad16b858a558c39d7fe91c92745999db570d74
hedgenull <hedgefleming@gmail.com>
2022-06-28 09:33:32 -0400

    Fixed API bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
-# Get the API key from the .config file
-conf = File.open("../../.config")
-key = conf.read.chomp
-conf.close
-
 class HomeController < ApplicationController
+  # Get the API key from the .config file
+  conf = File.open(".config")
+  key = conf.read.chomp
+  conf.close
   @@api = StockQuote::Stock.new(api_key: key)
 
   def index
