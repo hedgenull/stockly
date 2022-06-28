ba9448d2924bdbb025d5e2f6502f49b10dbb8ab0
hedgenull <hedgefleming@gmail.com>
2022-06-28 11:06:25 -0400

    Continued work on stock API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # GET routes
   get "home/about"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "home#index"
+
+  # POST routes
+  post "/" => "home#index"
 end
